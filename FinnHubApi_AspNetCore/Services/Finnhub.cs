5f28b037e7fe7331ecec16e95f9527bccc558f6c
jmark <jaymark.carba@cit.edu>
2024-01-13 23:02:48 +0800

    Intialize services

---
+++ FinnHubApi_AspNetCore/Services/Finnhub.cs
@@ -0,0 +1,15 @@
+﻿namespace FinnHubApi_AspNetCore.Services
+{
+    public class Finnhub : IFinnhubService
+    {
+        public Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinnHubApi_AspNetCore/Services/IFinnhubService.cs
@@ -0,0 +1,8 @@
+﻿namespace FinnHubApi_AspNetCore.Services
+{
+    public interface IFinnhubService
+    {
+        Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol);
+        Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol);
+    }
+}
